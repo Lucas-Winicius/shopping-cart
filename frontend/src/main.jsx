6ab0a232fc691b6959f821dec6b055e259ce2b36
Abobora <lucasclucas7@gmail.com>
2023-02-13 11:57:00 -0300

    Paginas criadas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  children: [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/cart',
+      element: <Cart />,
+    },
+  ]
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
