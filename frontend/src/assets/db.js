4599d95b373ad057a8c36f3e82f3f76ce0e1e56d
Abobora <lucasclucas7@gmail.com>
2023-02-15 09:00:27 -0300

    Produtos adicionados

--- frontend/src/assets/db.js
+++ frontend/src/assets/db.js
@@ -1,5 +1,28 @@
 module.exports = {
     produtos: [
-        
+        {
+            name: 'Produto 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            price: 100.50,
+            imageUrl: 'https://i.imgur.com/ulJeQyt.png'
+        },
+        {
+            name: 'Produto 2',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            price: 50.5,
+            imageUrl: 'https://i.imgur.com/ulJeQyt.png'
+        },
+        {
+            name: 'Produto 3',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            price: 50,
+            imageUrl: 'https://i.imgur.com/ulJeQyt.png'
+        },
+        {
+            name: 'Produto 4',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            price: 50,
+            imageUrl: 'https://i.imgur.com/ulJeQyt.png'
+        },
     ]
 }
