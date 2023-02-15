d338b6d4ca917986319c0c71913639af642d6efc
Abobora <lucasclucas7@gmail.com>
2023-02-14 23:56:16 -0300

    Footer finalizado

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import { RiHeartFill } from 'react-icons/ri'
+import '../styles/footer.css'
+
+const Footer = () => (
+    <footer>
+        <p>Feito com <RiHeartFill id='heart' /> por <a href="https://www.instagram.com/sr_pumpkin_/" target="_blank" rel="noopener noreferrer" id='link'>Lucas</a></p>
+    </footer>
+)
+
+export default Footer
