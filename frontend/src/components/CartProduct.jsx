7eb269b40e1fa1ee09d8441b9ce8c1266ef5a67a
Abobora <lucasclucas7@gmail.com>
2023-02-17 11:54:32 -0300

    atulizações no carrinho

---
+++ frontend/src/components/CartProduct.jsx
@@ -0,0 +1,8 @@
+const CartProduct = props => {
+
+    return (
+        <h1>{JSON.stringify(props)}</h1>
+    )
+}
+
+export default CartProduct
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,7 +1,34 @@
+import { useOutletContext } from "react-router-dom";
+import CartProduct from "../components/CartProduct";
+import { MdOutlineAddShoppingCart } from "react-icons/md"
+import { Link } from 'react-router-dom'
+import "../styles/cart.css"
+
 const Cart = () => {
     document.title = 'Carrinho'
+
+    const {cart} = useOutletContext();
+    const cartItem = Array.from(cart)
+
     return (
-        <h1>Cart</h1>
+        <div className="cart">
+            <div className="products">
+                {
+                    cartItem.length > 0 && cartItem
+                        .map((cartItem, index) => (
+                            <CartProduct props={cartItem} key={index}/>
+                        ))
+                }{
+                    cartItem.length <= 0 && 
+                    <div className="emptyCart">
+                        <Link to="/">
+                        <p><MdOutlineAddShoppingCart id="cartAdd"/> Adicione um produto</p>
+                        </Link>
+                    </div>
+                }
+            </div>
+            <div className="prices"></div>
+        </div>
     )
 }
 
