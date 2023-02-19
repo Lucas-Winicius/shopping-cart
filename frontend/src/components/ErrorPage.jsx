b2d92c8b4cec870048294d3b60358284f0c60e6a
Abobora <lucasclucas7@gmail.com>
2023-02-19 15:33:58 -0300

    Titulo dinamico na pagina de erro

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -2,10 +2,16 @@ import { useRouteError } from "react-router-dom"
 import "../styles/errorPage.css"
 
 const ErrorPage = () => {
+    
     const error = useRouteError()
-    return <div id="error">
+    document.title = error.statusText;
+    
+
+    return (
+    <div id="error">
         <img src={`https://http.cat/${error.status}`} className="errorImage"/>
     </div>
+    )
 }
 
 export default ErrorPage
